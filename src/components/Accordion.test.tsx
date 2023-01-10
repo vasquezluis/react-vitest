6ae556453cb22c03b170bbdf94289f5960e2c1de
LuiiVasquez <luivasque95@gmail.com>
2023-01-09 23:24:35 -0600

    creating more tests

--- src/components/Accordion.test.tsx
+++ src/components/Accordion.test.tsx
@@ -2,23 +2,55 @@
 
 /* Tests unitarios: peque;as porciones de codigo que comprueban algo dentro del componente*/
 
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Accordion from "./Accordion";
 
 // describe para agrupar distintos test
 describe("Accordion", () => {
-  test("should add two numbers", () => {
-    // esta linea de codigo se le llama asercion
-    // comprobar el codigo
+  // Esto se va a a;adir antes de la ejecucion de cada test
+  beforeEach(() => {
     render(
       <Accordion title="Holis">
         <h3>My content</h3>
-        <p>Some content</p>
+        <p>Some xyz</p>
       </Accordion>
     );
+  });
+
+  test("should show the accordion component", () => {
+    // esta linea de codigo se le llama asercion
+    // comprobar el codigo
     // esperar a que exista un elemento con el texto
     expect(screen.getByText("Holis")).toBeDefined();
   });
+
+  // Comprueba que el contenido este oculto al iniciar la aplicacion
+  test("should not display the content at the start", () => {
+    // expresion regular para buscar siempre el texto 'content' y la i para que no este relacionado con mayus minus
+    expect(screen.queryByText(/content/i)).toBeNull();
+  });
+
+  // testear cuando se haga click en open
+  test("should display content when button is clicked", () => {
+    // crear una variable con el texto a esperarse
+    const button = screen.getByText(/Open/i);
+    // evento click en el boton
+    fireEvent.click(button);
+    // esperar que el contenido si aparezca despues del click
+    expect(screen.queryByText(/content/i)).toBeDefined();
+  });
+
+  // testear cuando se haga click en open y que el contenido desaparezca
+  test("should hide content when button is clicked", () => {
+    // crear una variable con el texto a esperarse
+    const button = screen.getByText(/Open/i);
+    // evento click en el boton para aparecer, pero luego desaparecer el contenido
+    fireEvent.click(button);
+    fireEvent.click(button);
+    // esperar que el contenido si aparezca despues del click
+    expect(screen.queryByText(/content/i)).toBeNull();
+  });
+
 });
 
 /* 
