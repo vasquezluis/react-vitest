91c8ad3c829a9712215850eeb5c8eaf75085702e
LuiiVasquez <luivasque95@gmail.com>
2023-01-09 19:50:50 -0600

    configuring globals & environments

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// solo necesario por trabajar con TypeScript
+/// <reference types="vitest" />
+/// <reference types="Vite/client" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // configurar el test con jsdom, pero hay mas
+  test: {
+    environment: "jsdom",
+    globals: true
+  },
+});
